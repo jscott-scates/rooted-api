db54d0222eefae6687c3fdfcd327f87eddca11d9
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-04-29 12:55:23 -0400

    Added Register ViewSet and URL pathways for login and registration

--- rootedproject/urls.py
+++ rootedproject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from rootedapi.models import *
@@ -10,6 +12,10 @@ router.register(r"users", Users,"user")
 router.register(r"sages",Sages, "sage")
 
 urlpatterns = [
-    path('', include(router.urls)),
-]
+    path("", include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("api-token-auth", obtain_auth_token),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
